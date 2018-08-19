fdf783403e7323f361e8ae5dbdce585a92451119
yangyang <yangyang_ats@163.com>
2018-08-19 23:50:32 +0800

    0819

---
+++ common/2.py
@@ -0,0 +1,14 @@
+from urllib import request,parse
+url = "http://httpbin.org/post"
+headers = {
+    #伪装一个火狐浏览器
+    "User-Agent":'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
+    "host":'httpbin.org'
+}
+dict = {
+    "name":"Germey"
+}
+data = bytes(parse.urlencode(dict),encoding="utf8")
+req = request.Request(url=url,data=data,headers=headers,method="POST")
+response = request.urlopen(req)
+print(response.read().decode("utf-8"))
